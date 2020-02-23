b9297e25b22bafd9b02dbc934957ee58de5d20f3
thegrandstaff <59798435+thegrandstaff@users.noreply.github.com>
2020-02-23 01:34:44 -0500

    Add files via upload

---
+++ testing_integration/src/testing_package/ListenerTest.java
@@ -0,0 +1,32 @@
+package testing_package;
+
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+
+public class ListenerTest
+{
+    /*
+    @Test
+    void test1()
+    {
+        System.out.println("This is Test1");
+        Assert.assertEquals("A", "A");
+    }
+
+    @Test
+    void test2()
+    {
+        System.out.println("This is Test2");
+        Assert.assertEquals("A", "B");
+    }
+
+    @Test
+    void test3()
+    {
+        System.out.println("This is Test3");
+        throw new SkipException("This is skip exception.");
+    }
+    */
+
+}
---
+++ testing_integration/src/testing_package/testing_class.java
@@ -0,0 +1,22 @@
+package testing_package;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class testing_class
+{
+    @BeforeClass
+    public void setUp() {
+        // code that will be invoked when this test is instantiated
+    }
+
+    @Test(groups = { "fast" })
+    public void aFastTest() {
+        System.out.println("Fast test");
+    }
+
+    @Test(groups = { "slow" })
+    public void aSlowTest() {
+        System.out.println("Slow test");
+    }
+}
